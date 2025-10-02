88447b1652f4f90ec9ccf74f2b81135094346f89
bittt <colemeyersiscool@gmail.com>
2025-10-02 17:17:27 -0400

    Update bookmark.js

--- bookmark.js
+++ bookmark.js
@@ -1,5 +1,1 @@
-javascript:(function(){
-  fetch('https://raw.githubusercontent.com/bittt0/Cee/main/main.js')
-    .then(response => response.text())
-    .catch(err => console.error('Error loading script:', err));
-})();
+javascript:(function(){fetch('https://raw.githubusercontent.com/bittt0/Cee/main/main.js').then(r=>r.text()).then(js=> (0,eval)(js)).catch(e=>console.error('Error:',e));})();
